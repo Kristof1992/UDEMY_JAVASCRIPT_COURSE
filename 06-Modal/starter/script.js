40c9cb14a1c9399559be49dcf9f0fc701356aa64
Kristof1992 <kristof92selecz@gmail.com>
2023-02-22 23:26:17 +0100

    Modal (short) Project - Complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(selected);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', modalMouseEvent);
+}
+
+btnCloseModal.addEventListener('click', modalMouseEvent);
+overlay.addEventListener('click', modalMouseEvent);
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
+
+// *** Event Handler ***
+// Toggle adds a css class when not there and removes it when there.
+function modalMouseEvent(event) {
+  if (event.type === 'click') {
+    modal.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+  }
+}
