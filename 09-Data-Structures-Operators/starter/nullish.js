90aef6563fc8f7a49a7e826c036bae89f4411d5a
Kristof1992 <kristof92selecz@gmail.com>
2023-03-16 10:44:40 +0100

    Nullish Coalescing Operator (??)

---
+++ 09-Data-Structures-Operators/starter/nullish.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Data
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+    order: function(starterIndex, mainIndex) {
+      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
+  
+    // *** Destructuring object on the fly and using it ***
+    orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+      console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta: function(ing1, ing2, ing3) {
+        console.log(`Here is uour delicious pasta with ${ing1} ${ing2} ${ing3}`);
+    },
+
+    orderPizza: function(mainIngredient, ...otherIngredients) {
+        console.log(`Here is your delicious pizza with main ingredient ${mainIngredient} and other ings ${otherIngredients}`);
+    },
+  
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
+    },
+  };
+
+restaurant.numGuests = 0;
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
