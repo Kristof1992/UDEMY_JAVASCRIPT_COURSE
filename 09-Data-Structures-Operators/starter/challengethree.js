ca314e56bc1661b4c146d068f6b873d7a4006539
Kristof1992 <kristof92selecz@gmail.com>
2023-04-21 12:39:55 +0200

    Strings

---
+++ 09-Data-Structures-Operators/starter/challengethree.js
@@ -0,0 +1,124 @@
+let openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const rest = new Map();
+
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest);
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+
+const arr = [1, 2];
+// Objects as keys can be stored in Maps
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+console.log(rest.get(document.querySelector('h1')));
+
+rest.clear;
+
+console.log('\n');
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct '],
+  [false, 'Try again'],
+]);
+
+const questions = new Map(Object.entries({ 1: 'a' }));
+console.log(question);
+
+// Convert Object to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+// Destructuring
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+console.log(question.get('correct'));
+
+// Mini Challenge
+const answer = 3;
+console.log(question.get(question.get('correct') === answer));
+
+// Convert Map to Array (Matrix)
+console.log([...question]);
+
+console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+console.log('\n');
+
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
+]);
+
+// 1
+const set = [...new Set(gameEvents.values())];
+console.log(set);
+
+// 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const gameTime = [...gameEvents.keys()];
+console.log(gameTime.pop() / gameEvents.size);
+
+// 4
+for (const [key, value] of gameEvents) {
+  console.log(
+    `${key < 45 ? '[FIRST HALF]' : '[SECOND HALF]'} ${key}: ${value}`
+  );
+}
