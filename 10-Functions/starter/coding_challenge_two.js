33e0e4e40d7bbff99de0a1ab50365463e1b3e24d
Kristof1992 <kristof92selecz@gmail.com>
2023-04-25 01:47:50 +0200

    Coding Challenge 2

---
+++ 10-Functions/starter/coding_challenge_two.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Closures Challenge
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  const body = document.querySelector('body');
+  body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
