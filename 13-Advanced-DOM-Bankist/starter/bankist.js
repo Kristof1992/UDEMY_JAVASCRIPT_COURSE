25552bf468053de827c1491646222d15d408f20d
Kristof1992 <kristof92selecz@gmail.com>
2023-05-28 15:16:16 +0200

    Implementing Smooth Scrolling

--- 13-Advanced-DOM-Bankist/starter/bankist.js
+++ 13-Advanced-DOM-Bankist/starter/bankist.js
@@ -74,3 +74,35 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// Coordinates are relative to the ViewPort
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // Modern way - Only works in modern browsers
+  // section1.scrollIntoView({ behavior: 'smooth' });
+});
