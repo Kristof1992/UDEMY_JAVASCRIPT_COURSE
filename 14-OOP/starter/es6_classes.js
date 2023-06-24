e6add56436c9731da87cf8892420371754215ab9
Kristof1992 <kristof92selecz@gmail.com>
2023-06-24 20:21:02 +0200

    Setters and Getters

--- 14-OOP/starter/es6_classes.js
+++ 14-OOP/starter/es6_classes.js
@@ -4,20 +4,34 @@
 // const PersonCL = class {};
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
   calcAge() {
     console.log(2023 - this.birthYear);
   }
+
+  get age() {
+    return 2023 - this.birthYear;
+  }
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 
 console.log(jessica);
-console.log(jessica.firstName, jessica.birthYear);
+// console.log(jessica.firstName, jessica.birthYear);
 
 jessica.calcAge();
 
@@ -25,12 +39,34 @@ console.log(jessica);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
-PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
-};
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
 
-jessica.greet();
+// jessica.greet();
 
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizens
 // 3. Classes are executed in strict mode
+
+// Getters and Setters
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
+
+console.log(jessica.age);
+
+console.log(jessica.fullName);
