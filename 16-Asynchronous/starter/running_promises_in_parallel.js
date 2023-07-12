0eaf8e30f74beae1cb97becabff6e2fb9fcee7e9
Kristof1992 <kristof92selecz@gmail.com>
2023-07-12 23:19:49 +0200

    Asynchronous JS - Running Promises in Parallel

---
+++ 16-Asynchronous/starter/running_promises_in_parallel.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+const apiKEY = '4ed4cb41df56474ba36e0f70c0fdd8e9';
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${
+              (+data.population / 1000000).toFixed(1) + 'M'
+            } people</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+        </div>
+      </article>
+      `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const newWhereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=${apiKEY}`
+      );
+    })
+    .then(res => res.json())
+    .then(data => {
+      const { city, country } = data.features?.[0]?.properties;
+      return fetch(
+        `https://restcountries.com/v3.1/name/${country.toLowerCase()}`
+      );
+    })
+    .then(response => {
+      if (!response.ok) throw new Error('Country not found');
+      return response.json();
+    })
+    .then(data => {
+      if (!data) throw new Error(`Illegal data received.`);
+      renderCountry(data[0]);
+    })
+    .catch(e => console.error(`${e.message} 💥`))
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+// `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=${apiKEY}`
+// ***** ASYNC/AWAIT *****
+// prettier-ignore
+const whereAmI = async function () {
+  try {
+    const curPosition_GEOLOCATION_API = await getPosition();
+    const { latitude: lat, longitude: lng } = curPosition_GEOLOCATION_API.coords;
+    const result_GEOAPIFY_API = await fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=${apiKEY}`);
+    if(!result_GEOAPIFY_API.ok) throw new Error('Problem getting location data');
+    const result_GEOAPIFY_API_JSON = await result_GEOAPIFY_API.json();
+    const { city, country } = result_GEOAPIFY_API_JSON.features?.[0]?.properties;
+    const result_REST_COUNTRIES_API = await fetch(`https://restcountries.com/v3.1/name/${country}`); // Blocks only in async!
+    if(!result_REST_COUNTRIES_API.ok) throw new Error('Problem getting country');
+    const result_REST_COUNTRIES_API_JSON = await result_REST_COUNTRIES_API.json();
+    renderCountry(result_REST_COUNTRIES_API_JSON[0]);
+    return `You are in ${city}, ${country}`;
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderError(`Something went wrong 💥 ${err.message}`);
+    throw err;
+  }
+};
+
+// console.log('1: Getting location');
+// whereAmI()
+// .then(() => console.log(`2: ${city}`))
+// .catch((err) => console.error(`2: ${err.message} 💥`))
+// .finally(()=> console.log('3: Finished getting location'));
+
+// (async function () {
+//   try {
+//     console.log('1: Getting location');
+//     const city = await whereAmI();
+//     console.log(`2: ${city}`);
+//   } catch (err) {
+//     console.error(`2: ${err.message} 💥`);
+//   } finally {
+//     console.log('3: Finished getting location');
+//   }
+// })();
+
+// prettier-ignore
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`,'');
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`,'');
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`,'');
+
+    const data = await Promise.all([
+        getJSON(`https://restcountries.com/v3.1/name/${c1}`,''),
+        getJSON(`https://restcountries.com/v3.1/name/${c2}`,''),
+        getJSON(`https://restcountries.com/v3.1/name/${c3}`,'')
+    ]);
+    const dataMap = data.map(d => d[0].capital);
+    
+    // data.forEach(data => renderCountry(data[0][0]));
+    
+    // console.log([data1.capital, data2.capital, data3.capital]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
