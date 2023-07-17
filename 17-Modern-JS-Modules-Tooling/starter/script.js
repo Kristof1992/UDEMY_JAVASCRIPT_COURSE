2dcdcccd0afa6332320e8ff22299d20871cc4692
Kristof1992 <kristof92selecz@gmail.com>
2023-07-17 17:53:51 +0200

    Modern JS - Top-level await

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,10 +1,17 @@
 // Importing Module
 // Named imports
-// import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(totalPrice, totalQuantity);
 
 console.log('Importing module');
+
+// console.log('Start fetching...');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+// console.log('Something');
 
 // Importing everything as a Module Object
 // import * as ShoppingCart from './shoppingCart.js';
@@ -12,10 +19,24 @@ console.log('Importing module');
 // console.log(ShoppingCart);
 
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-import { cart } from './shoppingCart.js';
-import add from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
+// import { cart } from './shoppingCart.js';
+// import add from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 5);
+// add('apples', 4);
 
-console.log(cart);
+// console.log(cart);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+  //   for (let i = 0; i < 5_000_000_000; i++) {} // Blocks UI
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,9 @@
 // Exporting Module
 console.log('Exporting module');
 // export '/.shoppingCart';
+console.log('Start fetching users EXP');
+await fetch('https://jsonplaceholder.typicode.com/posts');
+console.log('Finish fetching users EXP');
 
 // Variables declared in a module are scoped to the Module itself.
 // By default top level variables are private!!!
@@ -21,3 +24,4 @@ export default function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${product} ${quantity} added to cart`);
 }
+// for (let i = 0; i < 9_000_000_000; i++) {}
