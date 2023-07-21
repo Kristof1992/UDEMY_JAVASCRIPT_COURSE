400429bfc409981b7974967b7829368d7ae3aa41
Kristof1992 <kristof92selecz@gmail.com>
2023-07-21 19:47:59 +0200

    Forkify App - Loading a Recipe from API

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,4 +1,5 @@
 const recipeContainer = document.querySelector('.recipe');
+const API_KEY = 'ad997c36-4189-4341-bad9-b291058f999c';
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +12,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40?key=${API_KEY}`
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=${API_KEY}`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+showRecipe();
