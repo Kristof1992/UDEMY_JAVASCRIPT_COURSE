a3071042e34f2b0e708e81620f2b68d35bea047e
Kristof1992 <kristof92selecz@gmail.com>
2023-07-21 14:11:42 +0200

    Modern JS - Programming Principles/F.Progamming

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,6 @@
-var budget = [
+'use strict';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,58 +9,91 @@ var budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-var limits = {
+// Immutable Object - No new properties can be added to the object - Doesn't apply to nested objects
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+/**
+ *
+ * @param {*} value transaction value + or -
+ * @param {*} description  transaction name
+ * @param {*} user  user name
+ */
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+  // if the property name is the same as the variable name it will add the value to the proper object field.
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user }]
+    : state;
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
+const budget1 = addExpense(budget, spendingLimits, 1000, 'Pizza 🍕');
+console.log(budget1);
+const budget2 = addExpense(
+  budget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+console.log(budget2);
+const budget3 = addExpense(budget2, spendingLimits, 200, 'Stuff', 'Jay');
+console.log(budget3);
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+const currentBudget = budget
+  .map(el => el.value)
+  .reduce((acc, currentValue) => acc + currentValue);
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
+const checkExpenses2 = function (state, limits) {
+  return state.map(obj => {
+    return obj.value < -getLimit(limits, obj.user)
+      ? { ...obj, flag: 'limit' }
+      : obj;
+  });
+  // for (const entry of budget)
+  //   if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
-
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
+const checkExpenses = (state, limits) => {
+  state.map(obj => {
+    obj.value < -getLimit(limits, obj.user) ? { ...obj, flag: 'limit' } : obj;
+  });
 };
-check();
+checkExpenses(budget3);
 
-console.log(budget);
+// console.log(budget);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(obj => obj.value <= -bigLimit)
+    // .map(obj => `${obj.description.slice(-2)}`)
+    // .join(' / ');
+    .reduce((str, cur) => `${str} ${cur.description.slice(-2)} /`, '')
+    .slice(0, -1);
+  console.log(bigExpenses); // Console.logs are side effects
+  /* 
+  before
+  str = '/' + curEmoji
+  str += '/' + curEmoji
+  str += '/' + curEmoji
+  */
+  /*
+  after
+  str = curEmoji + '/'
+  str += curEmoji + '/'
+  str += curEmoji + '/'
+  remove last '/'
+  */
 };
+
+logBigExpenses(budget3, 500);
